07ff533e935387c4fa79bf954093d7096b3239eb
schulzsebastian <schulz.siwy@gmail.com>
2016-07-31 18:30:01 +0200

    test to geojson

--- test_gispy.py
+++ test_gispy.py
@@ -16,28 +16,37 @@ class PointTestCase(unittest.TestCase):
         reverse = reprojected.reproject('2180', '4326')
         first = [round(_) for _ in self.point.coords()]
         second = [round(_) for _ in reverse.coords()]
-        self.assertEquals(first, second)
+        self.assertEqual(first, second)
 
     def test_to_geojson(self):
-        geojson = self.point.to_geojson()
+        path = './test'
+        filename = 'test'
+        geojson = self.point.to_geojson(path=path, filename=filename)
         dict_geojson = json.loads(geojson)
         dict_coords = dict_geojson['features'][0]['geometry']['coordinates']
-        self.assertEquals(dict_coords, self.point.coords())
+        with open(path + '/' + filename + '.geojson', 'r') as infile:
+            in_str = ''
+            for row in infile:
+                in_str += row.rstrip()
+            out_dict = json.loads(in_str)
+            out_coords = out_dict['features'][0]['geometry']['coordinates']
+        self.assertEqual(Point(out_coords).coords(), self.point.coords())
+        self.assertEqual(dict_coords, self.point.coords())
 
     def test_to_text(self):
         to_text = self.point.to_text()
         point = Point([float(_) for _ in to_text.split(';')])
-        self.assertEquals(point.coords(), self.point.coords())
+        self.assertEqual(point.coords(), self.point.coords())
 
     def test_to_csv(self):
         to_csv = self.point.to_csv()
         point = Point([float(_) for _ in to_csv.split(';')])
-        self.assertEquals(point.coords(), self.point.coords())
+        self.assertEqual(point.coords(), self.point.coords())
 
     def test_to_shp(self):
         to_shp = self.point.to_shp()
         point = Point([float(_) for _ in to_shp.split(';')])
-        self.assertEquals(point.coords(), self.point.coords())
+        self.assertEqual(point.coords(), self.point.coords())
 
 if __name__ == '__main__':
     unittest.main()
