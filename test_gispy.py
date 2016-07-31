8e494da6650d07d5ab6c68d03d96aa167dc5d3bf
schulzsebastian <schulz.siwy@gmail.com>
2016-07-31 18:17:43 +0200

    test function returns

--- test_gispy.py
+++ test_gispy.py
@@ -14,9 +14,30 @@ class PointTestCase(unittest.TestCase):
     def test_reproject(self):
         reprojected = self.point.reproject('4326', '2180')
         reverse = reprojected.reproject('2180', '4326')
-        first = [round(x) for x in self.point.coords()]
-        second = [round(x) for x in reverse.coords()]
+        first = [round(_) for _ in self.point.coords()]
+        second = [round(_) for _ in reverse.coords()]
         self.assertEquals(first, second)
+
+    def test_to_geojson(self):
+        geojson = self.point.to_geojson()
+        dict_geojson = json.loads(geojson)
+        dict_coords = dict_geojson['features'][0]['geometry']['coordinates']
+        self.assertEquals(dict_coords, self.point.coords())
+
+    def test_to_text(self):
+        to_text = self.point.to_text()
+        point = Point([float(_) for _ in to_text.split(';')])
+        self.assertEquals(point.coords(), self.point.coords())
+
+    def test_to_csv(self):
+        to_csv = self.point.to_csv()
+        point = Point([float(_) for _ in to_csv.split(';')])
+        self.assertEquals(point.coords(), self.point.coords())
+
+    def test_to_shp(self):
+        to_shp = self.point.to_shp()
+        point = Point([float(_) for _ in to_shp.split(';')])
+        self.assertEquals(point.coords(), self.point.coords())
 
 if __name__ == '__main__':
     unittest.main()
